a7eaa98766702b208e45395756bf96c3dc5d3bf7
TarasKarabas <taraskyparenko@gmail.com>
2023-05-08 14:41:56 +1030

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Taras Kyparenko on 8/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
