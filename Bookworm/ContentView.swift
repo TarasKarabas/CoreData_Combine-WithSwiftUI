c4246a1312df7c51cf0bdca2b51860e025b2d148
TarasKarabas <taraskyparenko@gmail.com>
2023-05-09 11:55:54 +1030

    commit

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+            }
+            Button("Add") {
+                let firstName = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastName = ["Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstName.randomElement()!
+                let chosenLastName = lastName.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +38,51 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//struct ContentView: View {
+////    @AppStorage("notes") private var notes = ""
+//    @State private var rememberMe = false
+//
+//    var body: some View {
+////        NavigationView {
+////            VStack{
+////                TextEditor(text: $notes)
+////                    .navigationTitle("Notes")
+////                    .padding()
+////            }
+//            VStack {
+//                PushButton(title: "Remember Me", isOn: $rememberMe)
+//                Text(rememberMe ? "ON" : "OFF")
+//            }
+////        }
+//    }
+//
+//    struct PushButton: View {
+//        let title: String
+//        @Binding var isOn: Bool
+//
+//        var onColors = [Color.red, Color.yellow]
+//        var offColors = [Color(white: 0.6), Color(white: 0.4)]
+//
+//        var body: some View {
+//            Button(title) {
+//                isOn.toggle()
+//            }
+//            .padding()
+//            .background(
+//                LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors),
+//                               startPoint: .top,
+//                               endPoint: .bottom))
+//            .foregroundColor(.white)
+//            .clipShape(Capsule())
+//            .shadow(radius: isOn ? 0 : 5)
+//        }
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
